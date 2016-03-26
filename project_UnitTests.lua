fd776e6da125635b7f5bad9fe72ff64d041a017b
Xipiryon <louis.schnellbach@gmail.com>
2016-03-26 13:58:46 +0100

    Updated Unit Tests project

--- project_UnitTests.lua
+++ project_UnitTests.lua
@@ -5,13 +5,21 @@
 project "UnitTests"
 	local ProjectRoot = os.getcwd()
 
+	dependson("HaizeLibrary")
 	language "C++"
 	targetname "UnitTests"
 	targetdir (SolutionRoot.."/bin")
 	kind "ConsoleApp"
 
+	if os.is("windows") then
+		-- Because on Windows, you can't start a program if .dll are not in the same folder...
+		postbuildcommands { string.gsub("copy "..SolutionRoot.."/bin/lib/*.dll "..SolutionRoot.."/bin/", "/", "\\") }
+	end
+
 	files	{
 		ProjectRoot.."/unittests/main.cpp",
+		ProjectRoot.."/unittests/tinyxml2.cpp",
+		ProjectRoot.."/unittests/tinyxml2.h",
 	}
 
 	filter "Debug*"
