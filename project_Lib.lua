361545f9cf75156d163a499f44657a5ad8ab7fce
Xipiryon <louis.schnellbach@gmail.com>
2016-04-01 08:39:06 +0200

    Copy dll from bin/lib/ to bin/

--- project_Lib.lua
+++ project_Lib.lua
@@ -13,6 +13,12 @@ project "IlargiaLibrary"
 		linkoptions {"-ldl"}
 	end
 
+	if os.is("windows") then
+		postbuildcommands { string.gsub("copy bin/lib/Ilargia*.dll bin/", "/", "\\") }
+	else
+		postbuildcommands { "cp bin/lib/Ilargia*.so bin/" }
+	end
+
 	files {
 		ProjectRoot.."/src/**.cpp",
 		ProjectRoot.."/include/**.hpp",
