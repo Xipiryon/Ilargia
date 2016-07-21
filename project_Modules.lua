6ba425c57fd6f5ecc584d0cfdb5e908152b6f645
Louis Schnellbach <xipiryon@gmail.com>
2016-07-21 10:58:16 +0000

    Automatically add modules from './modules/' folder

--- project_Modules.lua
+++ project_Modules.lua
@@ -2,21 +2,28 @@
 -- Modules
 -------------------------------------------
 
+function scandir(directory)
+	local i, t, popen = 0, {}, io.popen
+	local pfile = popen('ls -a "'..directory..'"')
+	for filename in pfile:lines() do
+		if filename ~= "." and filename ~= ".." then
+			i = i + 1
+			t[i] = filename
+		end
+	end
+	pfile:close()
+	return t
+end
+
 -- Specify the list of module to build within the engine
 -- You can simply comment / uncomment to build / unbuild
-G_ModuleToBuild = {
-	{ ["Folder"] = "ColorConsole",		["Name"] = "?"},
-	{ ["Folder"] = "OpenGLRenderer",	["Name"] = "?"},
-	{ ["Folder"] = "InputConsole",		["Name"] = "?"},
-	--"LogHTML"
-	--"Network"
-}
+G_ModuleToBuild = {}
 
 print('Building additional modules...')
-for _,project in pairs(G_ModuleToBuild) do
+for _,project in pairs(scandir('modules/')) do
 	-- Load project file
-	dofile("./modules/"..project.Folder.."/project_Lib.lua");
-	-- Extract Project name
-	project.Name = G_ProjectName;
-	print('... '..project.Name)
+	dofile("modules/"..project.."/project_Lib.lua");
+	-- Extract Project name and Project folder
+	table.insert(G_ModuleToBuild, { ["Folder"] = project, ["Name"] = G_ProjectName } )
+	print('... '..G_ProjectName)
 end
